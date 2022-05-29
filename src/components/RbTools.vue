c01beb1f735df7f62a98d95403a73b36d3da1edd
Midolii <462885536@qq.com>
2022-05-29 23:16:00 +0800

    costRecord

---
+++ src/components/RbTools.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="rb">
+    <div><go-top /></div>
+    <div><add-cost-record /></div>
+  </div>
+</template>
+
+<script setup>
+import GoTop from "@/components/goTop";
+import AddCostRecord from "@/components/addCostRecord";
+</script>
+
+<style lang="scss" scoped>
+.rb {
+  position: fixed;
+  right: 32px;
+  bottom: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  div{
+    margin: 2px;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/components/addCostRecord.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="add_record">Add</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.add_record {
+  height: 42px;
+  width: 42px;
+  padding: 6px;
+  opacity: 0.7;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(255, 255, 255);
+  box-shadow: 3px 3px 10px rgb(0 0 0 / 10%);
+  z-index: 999;
+  transition: opacity 0.3s ease;
+}
+.add_record:hover {
+  opacity: 1;
+  transition: opacity 0.3s ease;
+}
+</style>
